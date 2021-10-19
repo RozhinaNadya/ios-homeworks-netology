db0b129423a84519dc6a3995a8eb4fde4a074b87
RozhinaNadya <zhuk.09@mail.ru>
2021-10-19 18:06:22 +0300

    сделали необходимые изменениия в файле SceneDelegat, чтобы работать без сторибордов

--- Navigation/AppDelegate.swift
+++ Navigation/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
 
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
@@ -27,7 +26,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
 
     }
-
 
 }
 
