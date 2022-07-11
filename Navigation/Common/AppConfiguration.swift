d517deb7b4d3e4f45783e44cf5f16bbd02449477
RozhinaNadya <zhuk.09@mail.ru>
2022-07-11 11:55:40 +0200

    Added enum AppConfiguration

---
+++ Navigation/Common/AppConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  AppConfiguration.swift
+//  Navigation
+//
+//  Created by Надежда on 2022-07-11.
+//
+
+import UIKit
+
+enum AppConfiguration {
+    case people(urlString: String)
+    case starships(urlString: String)
+    case planets(urlString: String)
+}
