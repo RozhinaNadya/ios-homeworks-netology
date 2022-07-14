93ff40bba4d6f88aec6eeb139496d5559851891e
RozhinaNadya <zhuk.09@mail.ru>
2022-07-14 19:51:23 +0200

    Added UILabel to info screen, added func with JSONSerialization for request label.text

--- Navigation/Feed/Info/ViewInfo.swift
+++ Navigation/Feed/Info/ViewInfo.swift
@@ -14,18 +14,56 @@ class ViewInfo: UIView {
         return button
     }()
     
+    var requestLabel: UILabel = {
+        let label = UILabel()
+        label.toAutoLayout()
+        return label
+    }()
+    
+    private let networkService: NetworkServiceInfoProtocol = NetworkServiceInfo()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(buttonInfo)
         configureLayoutButtonInfo()
+        fetchData(urlString: "https://jsonplaceholder.typicode.com/todos/10")
+    }
+    
+    private func fetchData(urlString: String) {
+        guard let url = URL(string: urlString) else {return}
+        self.networkService.request(url: url) {
+            result in
+            switch result {
+            case .success(let data):
+                print("SUCCESS", String(data: data, encoding: .utf8))
+                do {
+                    let object = try JSONSerialization.jsonObject(with: data, options: [])
+                    print("OBJECT", object)
+                    
+                    if let dictionary = object as? [String: Any] {
+                        guard let textLabel = dictionary["title"] else {return}
+                        self.requestLabel.text = textLabel as? String
+                    }
+                } catch let error {
+                    print("ERROR", error)
+                }
+            case .failure(let error):
+                print("FAILURE", error)
+            }
+        }
     }
     
     func configureLayoutButtonInfo() {
+        addSubviews([buttonInfo, requestLabel])
         let constrInfo: [NSLayoutConstraint] = [
             buttonInfo.centerYAnchor.constraint(equalTo: centerYAnchor),
             buttonInfo.centerXAnchor.constraint(equalTo: centerXAnchor),
             buttonInfo.heightAnchor.constraint(equalToConstant: 50),
-            buttonInfo.widthAnchor.constraint(equalToConstant: 200)
+            buttonInfo.widthAnchor.constraint(equalToConstant: 200),
+            
+            requestLabel.topAnchor.constraint(equalTo: buttonInfo.bottomAnchor, constant: 20),
+            requestLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            requestLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -20),
+
         ]
         NSLayoutConstraint.activate(constrInfo)
     }
